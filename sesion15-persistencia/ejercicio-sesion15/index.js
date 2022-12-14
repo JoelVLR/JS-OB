5059fe0011005faabd3562f90bb490142acc8306
JoelVLR <jvlopezrojas@gmail.com>
2022-12-13 21:05:30 -0300

    sesion15

---
+++ sesion15-persistencia/ejercicio-sesion15/index.js
@@ -0,0 +1,12 @@
+const nombre = "Joel"
+const apellido="Lopez"
+const dato = {
+    nombre,
+    apellido,
+}
+
+//sessionStorage.setItem("objeto",JSON.stringify(dato))
+
+//localStorage.setItem("objeto",JSON.stringify(dato))
+
+//document.cookie=  "objeto="+JSON.stringify(dato) +";max-age="+120
